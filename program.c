f59364a1860e83a781e37726d17cefd3f65171d3
Adam Polinski <adampolinski22@gmail.com>
2025-01-24 09:40:40 +0100

    Kula

--- program.c
+++ program.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
 float cuboid_volume(float a, float b, float h) {
-    return(a*b*h);
-}
-int main() {
-    float a, b, h;
 
-    printf ("Podaj dlugosc podstawy prostopadloscianu: ");
+return(a*b*h);
+
+}
+float ball_volume(float r)
+{
+    return((4.0/3.0)*3.1415)*(r*r*r);
+}
+
+int main()
+{
+    float a, b, h, r;
+    printf("Podaj promien kuli\n");
+    scanf("%f", &r);
+   /* printf("podaj dlugosc podstawy prostopadloscianu\n");
     scanf("%f", &a);
 
-    printf ("Podaj szerokosc podstawy prostopadloscianu: ");
+    printf("podaj szerokosc podstawy prostopadloscianu\n");
     scanf("%f", &b);
 
-    printf ("Podaj wysokosc prostopadloscianu: ");
+    printf("podaj wysokosc prostopadloscianu\n");
     scanf("%f", &h);
 
-    printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
+    printf("\nObjetosc prostopadloscianu: %f", cuboid_volume(a,b,h)); */
+    printf("\nObjentosc kuli: %f", ball_volume(r));
 
-    return(0);
+    return 0;
 }
