df83b13c0b2e14b584424fd0545cf3afd7d07e46
Adam Polinski <adampolinski22@gmail.com>
2025-01-24 10:10:38 +0100

    Dodano menu

--- program.c
+++ program.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
-
-float cuboid_volume(float a, float b, float h) {
-
-return(a*b*h);
-
+#include <stdlib.h>
+float cuboid_volume(float a, float b, float h){
+return (a*b*h);
 }
-float ball_volume(float r)
-{
-    return((4.0/3.0)*3.1415)*(r*r*r);
+float ball_volume(float r){
+return(((4.0/3.0)*3.14)*(r*r*r));
 }
-
 int main()
 {
-    float a, b, h, r;
-    printf("Podaj promien kuli\n");
-    scanf("%f", &r);
-   /* printf("podaj dlugosc podstawy prostopadloscianu\n");
-    scanf("%f", &a);
+    float a, b, h,r;
+    int wybor;
+    printf ("Wybierz objetosc ktora chcesz obliczac\n");
+    printf ("1. objetosc prostopadloscian\n");
+    printf ("2. objetosc kuli\n");
+    printf ("wybor: ");
+    scanf ("%i", &wybor);
 
-    printf("podaj szerokosc podstawy prostopadloscianu\n");
-    scanf("%f", &b);
+    if (wybor==2)
+    {
+    printf ("Podaj promien kuli\n");
+    scanf ("%f", &r);
+    printf("\nObjetosc kuli: %f", ball_volume(r));
 
-    printf("podaj wysokosc prostopadloscianu\n");
-    scanf("%f", &h);
+    }
+    else if (wybor==1)
+    {
+   printf("Podaj dlugosc podstawy prostopadloscianu\n");
+   scanf("%f", &a);
 
-    printf("\nObjetosc prostopadloscianu: %f", cuboid_volume(a,b,h)); */
-    printf("\nObjentosc kuli: %f", ball_volume(r));
+   printf("Podaj szerokosc podstawy prostopadloscianu\n");
+   scanf("%f", &b);
+
+   printf("Podaj wysokosc podstawy prostopadloscianu\n");
+   scanf("%f", &h);
+
+   printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
+    }
+
 
     return 0;
 }
